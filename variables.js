9bc763ab8dfa45449102cc0037d11888f7d9dde5
Michael Lobato <mike.lobato.wft@gmail.com>
2022-09-20 15:56:23 -0700

    Node variables

--- variables.js
+++ variables.js
@@ -1,8 +1,14 @@
 var suffix1 = "Mr.";
 var suffix2 = "Mrs.";
-var suffix3 = Ms.
-var 30 = 30;
-var 40 = 40;
-var 50 = 50;
+var suffix3 = "Ms.";
+var num1 = 30;
+var num2 = 40;
+var num3 = 50;
 var earthisround = true;
 var earthisflat = false;
+suffix1 = "man";
+suffix2 = "married woman";
+suffix3 = "not married woman";
+console.log(suffix1)
+console.log(num1)
+console.log(earthisflat)
