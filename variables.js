6b575e9ee6a055f6f39b60c20fd1fd8a5e2b5843
Michael L, (he/him), FE <110502109+mrlobatoman@users.noreply.github.com>
2022-09-21 09:39:04 -0700

    Update variables.js

--- variables.js
+++ variables.js
@@ -4,11 +4,11 @@ var suffix3 = "Ms.";
 var num1 = 30;
 var num2 = 40;
 var num3 = 50;
-var earthisround = true;
-var earthisflat = false;
+var earthIsRound = true;
+var earthIsFlat = false;
 suffix1 = "man";
 suffix2 = "married woman";
 suffix3 = "not married woman";
 console.log(suffix1)
 console.log(num1)
-console.log(earthisflat)
+console.log(earthIsFlat)
